9c56fcb464695fe92722a8c67a225c094f3e7fd8
Vaidehi Venkatesan <vaidehi.venkatesan@gmail.com>
2017-06-20 00:36:40 -0400

    Whats wrong with flipping images

--- Py/Proj5_VehicleDetection/Vehicle_Detection_Application/VDetectApp/DataUtils/DataLoader.py
+++ Py/Proj5_VehicleDetection/Vehicle_Detection_Application/VDetectApp/DataUtils/DataLoader.py
@@ -7,9 +7,8 @@ class DataLoader:
 		car_images_regex = "./data/sample_vehicles/*/*.png";
 		car_images = glob.glob(car_images_regex);
 		self.car_images = car_images;
-		print(car_images)
 		
 		non_car_images_regex = "./data/sample_non-vehicles/*/*.png"
 		non_car_images = glob.glob(non_car_images_regex)
 		self.non_car_images = non_car_images;
-		print(non_car_images)
+		
